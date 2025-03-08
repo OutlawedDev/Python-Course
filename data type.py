8797f0cb111925a967bbbd22a326c87808cf6a88
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-08 12:11:50 +0500

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,9 @@
+a = 8
+b = "Hello"
+c =  True
+d = 2.5
+
+print("The data type of the a variable is", type(a))
+print("The data type of the b variable is", type(b))
+print("The data type of the c variable is", type(c))
+print("The data type of the d variable is", type(d))
