eb8e0c1f9eee16e222a33d6840f8ecf70f89a76c
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-16 13:45:18 +0500

    Add files via upload

---
+++ smaller.py
@@ -0,0 +1,9 @@
+num = int(input("Enter Your number to be checked"))
+
+if num < 15:
+    print("num is a smaller number than 15")
+    print("This is inside the if statement")
+else:
+    print("This number is greater than 15")
+    print("it is inside the else block")
+print("This statement is not inside the if or else block")
