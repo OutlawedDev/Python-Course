e56cd15fc7f5d675e9350b20af7ff0c406c58115
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-22 12:21:21 +0500

    Add files via upload

---
+++ not operator.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+c = 20
+
+print(a != b)
+print(b != c)
+
+a = "coding"
+b = "codingal"
+
+if a != b:
+    print(a, 'and',b , 'are different')
+
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print("hello")
+
+a = int(input("enter a number"))
+
+if a%2 != 0:
+    print(a, "is not an even number")
+else:
+    print(a, "is a even number")
