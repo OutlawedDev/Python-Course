e71f7abb3cc6507049f8c8a5e74d51c120d14477
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-22 12:08:47 +0500

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,22 @@
+a = 10
+b = 20
+c = 30
+
+if a and b and c:
+    print("All are equal to true")
+else:
+    print("All are equal to false")
+
+a = -1
+b = 2
+c = 0
+
+if a > 0 or b > 0:
+    print("either one of the value is greater than zero")
+else:
+    print("No number is greater than zero")
+
+if a > 0 or c > 0:
+    print("either one of the value is greater than zero")
+else:
+    print("No number is greater than zero")
