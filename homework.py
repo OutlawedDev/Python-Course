b7b7bac16afbc6a4ee905f1a1d71d772e37c31aa
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-22 11:31:50 +0500

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,7 @@
+temperature = int(input("Enter the temperature: "))
+if temperature > 25:
+    print("It's a hot day")
+
+speed = int(input("Enter your speed: "))
+if speed > 50:
+    print("You are speeding")
