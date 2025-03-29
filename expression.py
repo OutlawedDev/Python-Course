9057efa48cc97fcba1094555b53344a5523d890d
OutlawedDev <abdulhadiarain78@gmail.com>
2025-03-29 12:01:46 +0500

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,16 @@
+a = 8
+b = 5
+c = 7
+d = 3
+e = 7
+
+z = (b+c) * c / d;
+print("The answer of (b+c) * c / d is", z)
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "john" and age >= 2:
+    print("Hello, How are you")
+else:
+    print("Hello! Goodbye")
