28cda9b2181c930510bb58e24a63a52fc8799b1d
OutlawedDev <abdulhadiarain78@gmail.com>
2025-04-19 12:31:20 +0500

    Add files via upload

---
+++ homeworkn.py
@@ -0,0 +1,13 @@
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent (power): "))
+
+
+result = 1
+
+for _ in range(abs(exponent)):
+    result *= base
+
+if exponent < 0:
+    result = 1 / result
+
+print(f"{base} raised to the power of {exponent} is: {result}")
