8e8c29bf791d222fcc975e6049d9b2cddfc23f39
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-06-01 13:11:37 +0500

    Create randomtime.py

---
+++ randomtime.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+def getRandomDate(startDate, endDate ):
+    
+    print("Printing random Date between", startDate, "and", endDate)
+    randomGenerator = random.random()
+    
+    dateFormat = "%m/%d/%Y"
+    
+    
+    startTime = time.mktime(time.strptime(startDate, dateFormat))
+    endTime = time.mktime(time.strptime(endDate, dateFormat))
+    
+    
+    randomTime = startTime + randomGenerator * (endTime - startTime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+    
+print("random date = ", getRandomDate("1/1/2016", "12/12/2018"))
