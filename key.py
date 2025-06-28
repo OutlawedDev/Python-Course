dc0b15609358d67f113420b07e71583db89e340e
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-06-28 12:26:36 +0500

    Add files via upload

---
+++ key.py
@@ -0,0 +1,11 @@
+test_dict = {'codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'coding': 1}
+print("The Original Dictionary : " + str(test_dict))
+
+K = 2
+
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res+ 1
+
+print("Frequency of K is: " + str(res))
