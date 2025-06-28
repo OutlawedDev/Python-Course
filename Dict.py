d6cd24df7b97be49a5a2718ae0957dd475c2eee2
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-06-28 12:19:39 +0500

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,30 @@
+student_data = {'id1':
+                {'name': ['John Doe'],
+                'class': ['V'],
+                'subject integration': ['Maths, English, science'],
+                },
+                'id2':
+                {'name': ['David'],
+                'class': ['VI'],
+                'subject integration': ['SST, science, Maths'],
+                },
+                'id3':
+                {'name': ['Jane'],
+                'class': ['VII'],
+                'subject integration': ['English, SST, science'],
+                },
+                'id4':
+                {'name': ['Alice'],
+                'class': ['VIII'],
+                'subject integration': ['Maths, English, SST'],
+                },
+            }
+result = {}
+
+for key,value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
+
+                 
