eee837e93107c1737c0765147266b931c16fe039
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-06-28 12:33:52 +0500

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-print("Hello World!", 4)
-print("Welcome to codingal")
+SPCMcountry_code = {'India' : '0091',
+           'Austrailia' :'0061',
+           'Nepal' : '00977'}
+
+print("Country code for india -")
+
+print(SPCMcountry_code.get('India', 'Not Found'))
+
+
+print("Country code for Japan -")
+print(SPCMcountry_code.get ('Japan', 'Not Found'))                           
+
+                       
