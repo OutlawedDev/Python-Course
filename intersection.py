234332aa8f8d7a81a09fc0c696879b72c4398ac7
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-06-29 13:03:23 +0500

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+seta = {"blue", "green"}
+setb = {"red", "blue"}
+
+print("Original sets element: ", seta, setb)
+
+print("\nIntersection of two said sets: ")
+setz = seta.intersection(setb)
+print(setz)
