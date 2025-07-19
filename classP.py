3470e21a232ec216907f02615f41adc4d59e4125
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-07-19 12:37:51 +0500

    Add files via upload

---
+++ classP.py
@@ -0,0 +1,16 @@
+class parrot:
+
+    species = "Bird"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+blu = parrot("Blu", 10)
+woo = parrot("Woo", 15)
+
+print("blu is a {}".format(blu.species))
+print("woo is a {}".format(woo.species))
+
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
