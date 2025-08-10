3a023432861b7d64113091d76820a391e02d6a03
Agent_Saturo <abdulhadiarain78@gmail.com>
2025-08-10 13:08:16 +0500

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,27 @@
+from abc import ABC,abstractmethod
+
+
+class absclass(ABC):
+
+
+    def print(self, x):
+        print("passed value: ", x)
+
+    
+
+    @abstractmethod
+    def task(self):
+        print("We are inside the absclass task")
+
+
+
+class test_class(absclass):
+
+
+    def task(self):
+        print("We are inside the test_class task")
+
+
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)
